a6911e4184667744c1cc49ea7ec7d102664cf7e7
Frederic Flament <frederic.flament@usefulrisk.com>
2022-09-05 16:07:00 +0200

    Add files via upload

--- firstfile.go
+++ firstfile.go
@@ -3,5 +3,5 @@ package fftest0
 import "fmt"
 
 func Fromfftest0() {
-	fmt.Println("This comes from github.com/usefulrisk-datascience/fftest0")
+	fmt.Println("This comes from github.com/usefulrisk-datascience/fftest0 mais j'ai changé le texte")
 }
